9da77d9baa346b124f1a1b88339c9c0c832aeecd
sokolov <sokolov.ispp01@gmail.com>
2023-11-23 20:42:45 +0300

    model changes
    Добавлены значения по умолчанию в Discipline

--- app/src/main/java/com/twendev/vulpes/lagopus/model/Discipline.kt
+++ app/src/main/java/com/twendev/vulpes/lagopus/model/Discipline.kt
@@ -3,6 +3,6 @@ package com.twendev.vulpes.lagopus.model
 import com.google.gson.annotations.SerializedName
 
 data class Discipline (
-    @SerializedName("id"   ) val id   : Int,
-    @SerializedName("name" ) val name : String
+    @SerializedName("id"   ) val id   : Int = 0,
+    @SerializedName("name" ) val name : String = "Новая дисциплина"
 )
