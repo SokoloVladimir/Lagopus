1a27b5497b161995b76c41afd6e43209af617d16
sokolov <sokolov.ispp01@gmail.com>
2023-12-01 22:05:01 +0300

    InternalCheckBox update
    color balance

--- app/src/main/java/com/twendev/vulpes/lagopus/ui/component/checkbox/InternalCheckBox.kt
+++ app/src/main/java/com/twendev/vulpes/lagopus/ui/component/checkbox/InternalCheckBox.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
@@ -24,24 +23,39 @@ fun InternalCheckBox(
     checked: Boolean,
     enabled: Boolean,
     onCheckedChange: (Boolean) -> Unit,
+    animationDelayMs: Int = 350
 ) {
-    val primaryColor = MaterialTheme.colorScheme.primary
-    val secondaryColor = MaterialTheme.colorScheme.surface
+    val primaryColor = MaterialTheme.colorScheme.onPrimary
+    val secondaryColor = MaterialTheme.colorScheme.primary
+    val disabledPrimaryColor = MaterialTheme.colorScheme.secondary
+    val disabledSecondaryColor = MaterialTheme.colorScheme.secondaryContainer
 
     val bgColor = remember { Animatable(primaryColor) }
     val fgColor = remember { Animatable(secondaryColor) }
+    var disabledBgColor = if (checked) disabledPrimaryColor else disabledSecondaryColor
+    var disabledFgColor = if (!checked) disabledPrimaryColor else disabledSecondaryColor
 
     LaunchedEffect(checked) {
         if (checked) {
-            this.launch {
-                bgColor.animateTo(secondaryColor, animationSpec = tween(500))
+            launch {
+                bgColor.animateTo(secondaryColor, animationSpec = tween(animationDelayMs))
             }
-            fgColor.animateTo(primaryColor, animationSpec = tween(500))
+            launch {
+                fgColor.animateTo(primaryColor, animationSpec = tween(animationDelayMs))
+            }
+
+            disabledBgColor = disabledPrimaryColor
+            disabledFgColor =  disabledPrimaryColor
         } else {
-            this.launch {
-                bgColor.animateTo(primaryColor, animationSpec = tween(500))
+            launch {
+                bgColor.animateTo(primaryColor, animationSpec = tween(animationDelayMs))
             }
-            fgColor.animateTo(secondaryColor, animationSpec = tween(500))
+            launch {
+                fgColor.animateTo(secondaryColor, animationSpec = tween(animationDelayMs))
+            }
+
+            disabledBgColor = disabledSecondaryColor
+            disabledFgColor = disabledSecondaryColor
         }
     }
 
@@ -54,7 +68,7 @@ fun InternalCheckBox(
             }
             .border(1.dp, MaterialTheme.colorScheme.primary, MaterialTheme.shapes.small)
             .background(
-                color = if (enabled) bgColor.value else Color.LightGray,
+                color = if (enabled) bgColor.value else disabledBgColor,
                 shape = MaterialTheme.shapes.small
             )
             .padding(5.dp)
@@ -63,7 +77,7 @@ fun InternalCheckBox(
         Text(
             text = value,
             fontSize = 20.sp,
-            color = fgColor.value
+            color = if (enabled) fgColor.value else disabledFgColor
         )
     }
 }
