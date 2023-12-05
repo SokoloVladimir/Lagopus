070c200993488769881439e141c188c17fd995bb
sokolov <sokolov.ispp01@gmail.com>
2023-12-05 17:18:23 +0300

    added logo and incremented version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.twendev.vulpes.lagopus"
         minSdk = 25
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
