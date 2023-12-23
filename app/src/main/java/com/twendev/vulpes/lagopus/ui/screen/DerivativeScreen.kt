2f77074de75245ca1e3acc623857c6c7f4d89bb8
sokolov <sokolov.ispp01@gmail.com>
2023-12-23 14:28:54 +0300

    updated DerivativeScreenPreview
    добавлена обётка LagopusTheme которая и должна быть

--- app/src/main/java/com/twendev/vulpes/lagopus/ui/screen/DerivativeScreen.kt
+++ app/src/main/java/com/twendev/vulpes/lagopus/ui/screen/DerivativeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.dp
 import com.twendev.vulpes.lagopus.R
 import com.twendev.vulpes.lagopus.ui.NavigationManager
 import com.twendev.vulpes.lagopus.ui.TopAppBarElement
+import com.twendev.vulpes.lagopus.ui.theme.LagopusTheme
 
 @Composable
 fun DerivativeScreen(
@@ -66,9 +67,11 @@ fun DerivativeScreenContent(
 @Preview
 @Composable
 fun DerivativeScreenContentPreview() {
-    DerivativeScreenContent(
-        browseDisciplines = { },
-        browseWorkTypes = { },
-        browseSemesters = { }
-    )
+    LagopusTheme {
+        DerivativeScreenContent(
+            browseDisciplines = { },
+            browseWorkTypes = { },
+            browseSemesters = { }
+        )
+    }
 }
